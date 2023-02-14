79ff62c7fbb39ca6fd8de1bdfb6f50315b5c1684
Tanishq <119649887+ptsdpatient@users.noreply.github.com>
2023-02-14 19:40:30 +0530

    Update class-group-arrangement.cpp

--- class-group-arrangement.cpp
+++ class-group-arrangement.cpp
@@ -1,50 +1,42 @@
-#include<iostream>
-#include<windows.h>
-using namespace std;
-const int x = 11;
-const int y = 7;
-int n=70;
-int a[x],b[x],c[x],d[x],e[x],f[x],g[x];
+#include <iostream>
+#include<ctime>
 
-int randomGen(int mod) {
-	return rand()%mod;
+
+int list[7][10];
+int value = 70;
+int genrand(int lessThan) {
+	
+	int random=7;
+	random = rand() % lessThan;
+		return random;
 }
-void allotStudents() {
-	
-	for(int i = 0; i < 10; i++) {
-	
+void printlist() {
+	for (int i = 0; i < 7; i++) {
+		for (int j = 0; j < 10; j++) { std::cout << list[i][j] << ", "; }
+		std::cout << std::endl;
 	}
 }
-/* void allotStudents() {
-	for (int i = 0; i < 9; i++) {
-		switch (rand() % 8) {
-		case 1:a[i] = n;
-			break;
-		case 2:b[i] = n;
-			break;
-		case 3:c[i] = n;
-			break;
-		case 4:d[i] = n;
-			break;
-		case 5:e[i] = n;
-			break;
-		case 6:f[i] = n;
-			break;
-		case 7:g[i] = n;
-			break;
-		default:
-			break;
-
-		
+void initialize() {
+	for (int i = 0; i < 7; i++) {
+		for (int j = 0; j < 10; j++) { 
+			list[i][j] = 0;
 		}
-		n++;
+}
+}
+void assigngroup() {
+	srand(time(NULL));
+	int x, y;
+	while (value > 0) {
+		do { x = genrand(7); y = genrand(10); /*std::cout << x << ", " << y << "\n";*/ } while (!(list[x][y] == 0));
+		
+		list[x][y] = value;
+		std::cout << x << "," << y << "\t" << list[x][y] << "\n";
+		value--;
 	}
-} */
+}
 int main() {
-	allotStudents();
-	for (int i = 0; i < 10; i++) {
-		cout<<endl << a[i]<<" "<< b[i] <<" " << c[i] <<" " << d[i] <<" " << e[i] <<" " << f[i] <<" " << g[i];
-	    
-	}
+	initialize();
+	assigngroup();
+	printlist();
 	return 0;
 }
